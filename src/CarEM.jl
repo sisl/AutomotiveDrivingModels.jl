e66577ad6821f11fdd53b01daf5e4a609154b5ba
Tim Wheeler <timwheeleronline@gmail.com>
2014-11-20 14:36:40 -0800

    initial commit

--- src/CarEM.jl
+++ src/CarEM.jl
@@ -1,5 +1,26 @@
+# Car Encounter Model
+# 	largely based off of EM sampling code by Dr. Mykel Kochenderfer 
+# 	and translations by Youngjun Kim
+# See the encounter model representation in Dr. Kochenderfer's work on Aircraft Encounter Models
+
 module CarEM
 
-# package code goes here
+using BinMaps
+
+export EM
+
+immutable EM
+
+	labels   :: Vector{Symbol}          # node labels
+	n        :: Int                     # number of nodes
+	G        :: BitMatrix               # adjacency matrix. G[i,j] is true iff [i] is a parent of [j]
+	r        :: Vector{Int}             # bincounts. r[i] is the number of discrete values node i can take on
+	N        :: Vector{Matrix{Int}}     # sufficient statistics. N[i] is a r[i]×q[i] matrix
+	binmaps  :: Vector{AbstractBinMap}  # bin mappers
+
+	EM() = new()
+end
+
+
 
 end # module
