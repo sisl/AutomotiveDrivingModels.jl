8fbae51ae31d8c3dc5e35437c2da8b41d3d7c702
Tim Wheeler <wheelert@stanford.edu>
2015-09-23 14:51:13 -0700

    BN

---
+++ scripts/model_training/train_BN/spearmint_BN.py
@@ -0,0 +1,27 @@
+import os
+import subprocess
+import numpy as np
+
+def spearmint_BN(params):
+
+  directory = os.path.dirname(os.path.abspath(__file__))
+
+  ncandidate_bins = params['ncandidate_bins'][0]
+
+  bashCommand = 'julia ' + directory + '/train_BN.jl' \
+                ' ncandidate_bins ' + str(ncandidate_bins)
+  print bashCommand
+
+  process = subprocess.Popen(bashCommand.split(), stdout=subprocess.PIPE)
+  output = process.communicate()[0]
+  result = -float(output)
+
+  print 'Result = %f' % result
+
+  return {'main' : result}
+
+def main(job_id, params):
+  print 'Anything printed here will end up in the output directory for job #%d' % job_id
+  print params
+
+  return spearmint_BN(params)
