2d039959b3c3ef1f2ef8c408b54b361791f0edc6
Tim Wheeler <wheelert@stanford.edu>
2015-10-01 13:25:21 -0700

    it works

--- scripts/streetmap_extraction/gen_streetmaps.jl
+++ scripts/streetmap_extraction/gen_streetmaps.jl
@@ -1,30 +1,32 @@
 using AutomotiveDrivingModels
 using AutomotiveDrivingModels.StreetNetworks.RoadNetwork
 
-# for (RNDF_FILE, STREETMAP_OUTPUT_FILE) in (
-#         ("/media/tim/DATAPART1/Data/Bosch/source2/detroit_v4_rndf_copy.txt",
-#          "/media/tim/DATAPART1/Data/Bosch/processed/streetmaps/streetmap_detroit.jld"),
-#         ("/media/tim/DATAPART1/Data/Bosch/source2/its_both_loops_v3_smoothed.txt",
-#          "/media/tim/DATAPART1/Data/Bosch/processed/streetmaps/streetmap_its_both_loops_v3_smoothed.jld"),
-#         ("/media/tim/DATAPART1/Data/Bosch/source2/its_inner_loop_v1_2_rndf.txt",
-#          "/media/tim/DATAPART1/Data/Bosch/processed/streetmaps/streetmap_its_inner_loop_v1_2_rndf.jld"),
-#         ("/media/tim/DATAPART1/Data/Bosch/philippruns_20150324/280_pagemill_to_92.txt",
-#          "/media/tim/DATAPART1/Data/Bosch/processed/streetmaps/streetmap_280_pagemill_to_92.jld")
-#     )
+for (RNDF_FILE, STREETMAP_OUTPUT_FILE) in (
+        ("/media/tim/DATAPART1/Data/Bosch/source2/detroit_v4_rndf_copy.txt",
+         "/media/tim/DATAPART1/Data/Bosch/processed/streetmaps/streetmap_detroit_v4.jld"),
+        ("/media/tim/DATAPART1/Data/Bosch/source2/its_both_loops_v3_smoothed.txt",
+         "/media/tim/DATAPART1/Data/Bosch/processed/streetmaps/streetmap_its_both_loops_v3_smoothed.jld"),
+        ("/media/tim/DATAPART1/Data/Bosch/source2/its_inner_loop_v1_2_rndf.txt",
+         "/media/tim/DATAPART1/Data/Bosch/processed/streetmaps/streetmap_its_inner_loop_v1_2.jld"),
+        ("/media/tim/DATAPART1/Data/Bosch/philippruns_20150324/280_pagemill_to_92.txt",
+         "/media/tim/DATAPART1/Data/Bosch/processed/streetmaps/streetmap_280_pagemill_to_92.jld"),
+        ("/media/tim/DATAPART1/Data/Bosch/philippruns_20150324/280_pagemill_to_92.txt",
+         "/media/tim/DATAPART1/Data/Bosch/processed/streetmaps/streetmap_280_pagemill_to_92.jld")
+    )
 
-#     @printf("Loading RNDF... "); tic()
-#     rndf = RoadNetwork.load_rndf(RNDF_FILE)
-#     toc()
+    @printf("Loading RNDF... "); tic()
+    rndf = RoadNetwork.load_rndf(RNDF_FILE)
+    toc()
 
-#     println(methods(AutomotiveDrivingModels.rndf2streetnetwork))
-#     @printf("Building streetmap... "); tic()
-#     sn = AutomotiveDrivingModels.rndf2streetnetwork(rndf)
-#     toc()
+    println(methods(AutomotiveDrivingModels.rndf2streetnetwork))
+    @printf("Building streetmap... "); tic()
+    sn = AutomotiveDrivingModels.rndf2streetnetwork(rndf)
+    toc()
 
-#     @printf("Saving streetmap... "); tic()
-#     save(STREETMAP_OUTPUT_FILE, "streetmap", sn)
-#     toc()
-# end
+    @printf("Saving streetmap... "); tic()
+    save(STREETMAP_OUTPUT_FILE, "streetmap", sn)
+    toc()
+end
 
 # NOTE(tim): this was back when I was inferring the 280 RNDF from the curves obtained from Jorge
 
