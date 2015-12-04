ca11eb56da4891257532a571960ccba29aa4ee7d
Tim Wheeler <wheelert@stanford.edu>
2015-12-04 15:59:18 -0800

    updated

--- src/utils/RunLogs.jl
+++ src/utils/RunLogs.jl
@@ -3,7 +3,7 @@ module RunLogs
 using DataFrames
 using DataArrays
 
-import AutomotiveDrivingModels: LaneTag, CurvePt, VecSE2
+import AutomotiveDrivingModels: LaneTag, CurvePt, VecSE2, VecE2
 
 export
     AgentClass,                         # enum containing classes of agents
@@ -15,10 +15,12 @@ export
     LaneTag,                            # reexport from StreetNetworks
 
     ID_NULL,                            # null value for non-existant UID
+    ID_EGO,                             # ego UID
     COLSET_NULL,                        # null value for non-existant column set index
 
     # create_empty_runlog               # generate a new one
     # expand_rowcount!,                 # increase the number of rows
+    push_agent!,                        # add a new agent to the set
     push_columnset!,                    # adds a new column set (with id's = ID_NULL) and returns runlog
     extract_subset,                     # pull a subregion as a new runlog
 
@@ -31,7 +33,6 @@ export
 
     idinframe,                          # whether the given id is in the frame
     idinframes,                         # whether the given id is in all the frames
-    colsetinframe,                      # whether the given colset is in the frame
 
     colset2id,                          # get the id of the vehicle in the given colset
     id2colset,                          # get the colset of the vehicle with the given id, returns COLSET_NULL on fail
@@ -71,6 +72,7 @@ baremodule AgentClass
 end
 
 baremodule ControlStatus
+    const UNKNOWN             = 0x00 #
     const START               = 0x01 # vehicle is in start mode
     const ACTIVE              = 0x02 # vehicle is in active mode
     const ACTIVE_REINT        = 0x03 #
@@ -94,7 +96,7 @@ baremodule ContextClass
     const LANECHANGE          = 0x0004
 end
 
-type AgentDefinition
+immutable AgentDefinition
     class  :: UInt8   # agent class ∈ {motorcycle, car, ...} (see AgentClass)
     id     :: UInt    # unique identification number
     length :: Float64 # bounding box length [m]
@@ -133,31 +135,46 @@ type RunLog
     framedata::DataFrame # the data that is not repeated for each frame (time, environment)
     colsets::Vector{DataFrame} # each dataframe contains the same columns
                                # colsets is indexed by `colset`, the column set index
+
+    function RunLog(nframes::Integer, header::RunLogHeader, ncolsets::Integer=0)
+
+        agents = Dict{UInt, AgentDefinition}()
+        framedata = _create_framedata(nframes)
+        colsets = Array(DataFrame, ncolsets)
+
+        for i in 1 : ncolsets
+            colsets[i] = _create_columnset(nframes)
+        end
+
+        new(header, agents, framedata, colsets)
+    end
 end
 
 const ID_NULL = typemax(UInt)
+const ID_EGO = one(UInt)
 const COLSET_NULL = zero(UInt)
 
 # BODY
 # ----------------------------------------
 # EACH FRAME CONTAINS THESE VALUES ONCE
 #
-# Float64 time        [s] unix epoch
-# UInt16  environment [0] environment mask
+# Float64 time            [s] unix epoch
+# UInt16  environment     [0] environment mask
 # ----------------------------------------
 # THESE COLUMNS ARE REPEATED FOR N VEHICLES (each entry in RunLog.colsets)
 #
-# UInt        id        [-] id of the vehicle in this column set for this frame, ID_NULL if none
-# UInt        indnext   [-] column set index for this vehicle in the next frame, COLSET_NULL if none
-# UInt        indprev   [-] column set index for this vehicle in the previous frame, COLSET_NULL if none
-# VecSE2      inertial  [m,m,rad] vehicle position in inertial coordinates
-# VecSE2      ratesB    [m/s, m/s, rad/2] linear and angular rates in the body frame
-# VecSE2      ratesF    [m/s] linear rates in the lane-relative Frenet frame (relative to closest lane centerline)
-# CurvePt     footpoint [-] point on the closest centerline
-# LaneTag     lanetag   [-] assigned lane in given map
-# UInt        front     [-] colset of the vehicle in front of this one, COLSET_NULL if none
-# UInt        rear      [-] colset of the vehicle behind this one, COLSET_NULL if none
-# UInt16      behavior  [0] context class
+# UInt        id          [-] id of the vehicle in this column set for this frame, ID_NULL if none
+# UInt        next_colset [-] column set index for this vehicle in the next frame, COLSET_NULL if none
+# UInt        prev_colset [-] column set index for this vehicle in the previous frame, COLSET_NULL if none
+# VecSE2      inertial    (x,y,ϕ) [m,m,rad] vehicle position in inertial coordinates
+# VecSE2      frenet      (s,d,θ) [m,m,rad] vehicle position in frenet coordinates
+# VecSE2      ratesB      [m/s, m/s, rad/2] linear and angular rates in the body frame
+# VecS2       ratesF      [m/s, m/s] linear rates in the lane-relative Frenet frame (relative to closest lane centerline)
+# CurvePt     footpoint   [-] point on the closest centerline
+# LaneTag     lanetag     [-] assigned lane in given map
+# UInt        front       [-] colset of the vehicle in front of this one, COLSET_NULL if none
+# UInt        rear        [-] colset of the vehicle behind this one, COLSET_NULL if none
+# UInt16      behavior    [0] context class
 
 function _create_framedata(nframes::Integer)
     DataFrame(
@@ -168,11 +185,12 @@ end
 function _create_columnset(nframes::Integer)
     DataFrame(
         id = fill(ID_NULL, nframes),
-        indnext = fill(COLSET_NULL, nframes),
-        indprev = fill(COLSET_NULL, nframes),
+        next_colset = fill(COLSET_NULL, nframes),
+        prev_colset = fill(COLSET_NULL, nframes),
         inertial = DataArray(VecSE2, nframes),
+        frenet = DataArray(VecSE2, nframes),
         ratesB = DataArray(VecSE2, nframes),
-        ratesF = DataArray(VecSE2, nframes),
+        ratesF = DataArray(VecE2, nframes),
         footpoint = DataArray(CurvePt, nframes),
         lanetag = DataArray(LaneTag, nframes),
         idfront = fill(COLSET_NULL, nframes),
@@ -181,6 +199,11 @@ function _create_columnset(nframes::Integer)
     )
 end
 
+function push_agent!(runlog::RunLog, agent::AgentDefinition)
+    @assert(!haskey(runlog.agents, agent.id))
+    runlog.agents[agent.id] = agent
+    runlog
+end
 function push_columnset!(runlog::RunLog)
     df = _create_columnset(nframes(runlog))
     push!(runlog.colsets, df)
@@ -229,11 +252,94 @@ ncolsets(runlog::RunLog) = length(runlog.colsets)
 frame_inbounds(runlog::RunLog, frame::Integer) = (1 ≤ frame ≤ nframes(runlog))
 
 Base.get(runlog::RunLog, frame::Integer, sym::Symbol) = runlog.framedata[frame, sym]
-Base.get(runlog::RunLog, colset::UInt, frame::Integer, sym::Symbol) = runlog.colsets[convert(UInt, colset)][frame, sym]
+Base.get(runlog::RunLog, colset::UInt, frame::Integer, sym::Symbol) = runlog.colsets[colset][frame, sym]
+function Base.get{I<:Integer}(runlog::RunLog, frames::AbstractVector{I}, sym::Symbol)
+    retval = Array(eltype(runlog.framedata[sym]), length(frames))
+    for (i,frame) in enumerate(frames)
+        retval[i] = runlog.framedata[frame, sym]
+    end
+    retval
+end
+function Base.get{I<:Integer}(runlog::RunLog, colset::UInt, frames::AbstractVector{I}, sym::Symbol)
+    df = runlog.colsets[colset]
+    retval = Array(eltype(df[sym]), length(frames))
+    for (i,frame) in enumerate(frames)
+        retval[i] = df[frame, sym]
+    end
+    retval
+end
+
 function set!(runlog::RunLog, colset::UInt, frame::Integer, sym::Symbol, value::Any)
     runlog.colsets[colset][frame, sym] = value
     runlog
 end
+function set!{I<:Integer}(runlog::RunLog, colset::UInt, frames::AbstractVector{I}, sym::Symbol, values::AbstractVector)
+
+    df = runlog.colsets[colset]
+    for (frame,v) in zip(frames, values)
+        df[frame, sym] = value
+    end
+
+    runlog
+end
+function set!(
+    runlog::RunLog,
+    colset::UInt,
+    frame::Integer,
+    id        :: UInt,
+    inertial  :: VecSE2,
+    frenet    :: VecSE2,
+    ratesB    :: VecSE2,
+    footpoint :: CurvePt,
+    lanetag   :: LaneTag,
+    front     :: UInt = COLSET_NULL,
+    rear      :: UInt = COLSET_NULL,
+    behavior  :: UInt16 = ContextClass.NULL,
+    )
+
+    #=
+    Sets all attributes for a vehicle's frame
+    Note that next_colset, prev_colset, and ratesF are automatically computed
+    Note that front and rear are >not< computed
+    =#
+
+    df = runlog.colsets[colset]
+
+    next_colset = (frame < nframes(runlog)) ? id2colset(runlog, id, frame+1) : COLSET_NULL
+    prev_colset = (frame > 1) ? id2colset(runlog, id, frame-1) : COLSET_NULL
+
+    speed = hypot(ratesB)
+    ratesF = polar(speed, frenet.θ)
+
+    df[frame, :id         ] = id
+    df[frame, :next_colset] = next_colset
+    df[frame, :prev_colset] = prev_colset
+    df[frame, :inertial   ] = inertial
+    df[frame, :frenet     ] = frenet
+    df[frame, :ratesB     ] = ratesB
+    df[frame, :ratesF     ] = ratesF
+    df[frame, :footpoint  ] = footpoint
+    df[frame, :lanetag    ] = lanetag
+    df[frame, :front      ] = front
+    df[frame, :rear       ] = rear
+    df[frame, :behavior   ] = behavior
+
+    runlog
+end
+function set!(runlog::RunLog, frame::Integer, sym::Symbol, value::Any)
+    runlog.framedata[frame, sym] = value
+    runlog
+end
+function set!{I<:Integer}(runlog::RunLog, frames::AbstractVector{I}, sym::Symbol, values::AbstractVector)
+
+    df = runlog.framedata
+    for (frame,v) in zip(frames, values)
+        df[frame, sym] = value
+    end
+
+    runlog
+end
+
 
 function idinframe(runlog::RunLog, id::UInt, frame::Integer)
     # whether the given id is in the frame
@@ -255,30 +361,18 @@ function idinframes(runlog::RunLog, id::UInt, frames::AbstractVector{Int64})
     end
     true
 end
-function colsetinframe(runlog::RunLog, colset::UInt, frame::Integer)
-    # whether the given colset is in the frame
-
-    for (i,df) in enumerate(runlog.colsets)
-        if df[frame, :id] == ID_NULL
-            return false
-        end
-        if i == colset
-            return true
-        end
-    end
-    false
-end
 
 colset2id(runlog::RunLog, colset::UInt, frame::Integer) = get(runlog, colset, frame, :id)::UInt
 function id2colset(runlog::RunLog, id::UInt, frame::Integer)
 
     #=
     Find the colset for the given id in the frame
+    NOTE: does not check whether frame is valid
     Returns COLSET_NULL if non-existent
     =#
 
-    for colset = 1 : carindmax(runlog)
-        if carind2id(runlog, colset, frame) == id
+    for colset in 1 : carindmax(runlog)
+        if colset2id(runlog, colset, frame) == id
             return colset
         end
     end
@@ -288,14 +382,14 @@ end
 function estimate_framerate(runlog::RunLog)
 
     N = nframes(runlog)
-    t0 = runlog.framedata[1, :time]::Float64
-    tf = runlog.framedata[N, :time]::Float64
+    t0 = get(runlog, 1, :time)::Float64
+    tf = get(runlog, N, :time)::Float64
 
     (tf - t0) / (N-1)
 end
 function get_elapsed_time(runlog::RunLog, frameA::Integer, frameB::Integer)
-    t1 = runlog.framedata[frameA, :time]::Float64
-    t2 = runlog.framedata[frameB, :time]::Float64
+    t1 = get(runlog, frameA, :time)::Float64
+    t2 = get(runlog, frameB, :time)::Float64
     t2 - t1
 end
 function get_frame( runlog::RunLog, time::Float64, mode::Symbol=:nearest)
@@ -309,32 +403,32 @@ function get_frame( runlog::RunLog, time::Float64, mode::Symbol=:nearest)
     =#
 
     N = nframes(runlog)
-    t0 = runlog.framedata[1, :time]::Float64
-    tf = runlog.framedata[N, :time]::Float64
+    t0 = get(runlog, 1, :time)::Float64
+    tf = get(runlog, N, :time)::Float64
     Δt = (tf - t0) / (N-1)
 
     frame = clamp(round(Int, 1.0 + (time - t0) / Δt, RoundNearestTiesAway), 1, N)
-    t  = gete(pdset, :time, frame)::Float64
+    t  = get(runlog, frame, :time)::Float64
 
     # now do a search to ensure we are actually the closest we can be to that time
-    t⁺ = (frame < N) ? runlog.framedata[frame+1, :time]::Float64 : t
+    t⁺ = (frame < N) ? get(runlog, frame+1, :time)::Float64 : t
     if abs(t⁺ - time) < abs(t - time)
         t, frame = t⁺, frame+1
-        t⁺ = (frame < N) ? runlog.framedata[frame+1, :time]::Float64 : t
+        t⁺ = (frame < N) ? get(runlog, frame+1, :time)::Float64 : t
         while abs(t⁺ - time) < abs(t-time)
             t, frame = t⁺, frame+1
-            t⁺ = (frame < N) ? runlog.framedata[frame+1, :time]::Float64 : t
+            t⁺ = (frame < N) ? get(runlog, frame+1, :time)::Float64 : t
         end
         return frame
     end
 
-    t⁻ = (frame > 1) ? runlog.framedata[frame-1, :time]::Float64 : t
+    t⁻ = (frame > 1) ? get(runlog, frame+1, :time)::Float64 : t
     if abs(t⁻ - time) < abs(t-time)
         t, frame = t⁻, frame-1
-        t⁻ = (frame > 1) ? runlog.framedata[frame-1, :time]::Float64 : t
+        t⁻ = (frame > 1) ? get(runlog, frame+1, :time)::Float64 : t
         while abs(t⁻ - time) < abs(t-time)
             t, frame = t⁻, frame-1
-            t⁻ = (frame > 1) ? runlog.framedata[frame-1, :time]::Float64 : t
+            t⁻ = (frame > 1) ? get(runlog, frame+1, :time)::Float64 : t
         end
         return frame
     end
@@ -382,8 +476,6 @@ end
 # Base.delete!(runlog::RunLog, id::UInt) # remove the given id from all frames in runlog and return the runlog
     # this function should then shift all actors above colset down to fill the gap
     # note that this requires us to update indnext and indprev for the previous and next frames
-
-# add_columnset!(runlog::RunLog) # adds a new column set (with id's = ID_NULL) and returns runlog
 
 function get_first_vacant_colset(runlog::RunLog, id::UInt, frame::Integer)
     for (colset, df) in enumerate(runlog.colsets)
