7990dc2c3aaf9523bbe0ef38002509982d5bfc2c
Tim Wheeler <wheelert@stanford.edu>
2016-07-13 11:28:45 -0700

    removing old code

--- src/io/filesystem_utils.jl
+++
@@ -1,5 +0,0 @@
-# change the path to have the given extension
-toext(path::AbstractString, ext::AbstractString) = splitext(path)[1] * cleanext(ext)
-
-# append the dot for an extension if need be
-cleanext(ext::AbstractString) = ext[1] == '.' ? ext : "." * ext
