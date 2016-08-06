83f3beb52b16aa2c25dfcaaec7da94c4e968ab41
Tim Wheeler <wheelert@stanford.edu>
2016-08-06 11:17:01 -0700

    removing AutoViz test code

--- test/viz/test_viz.jl
+++
@@ -1,4 +0,0 @@
-include(Pkg.dir("AutomotiveDrivingModels", "src", "viz", "AutoViz.jl"))
-using AutoViz
-
-include(Pkg.dir("AutomotiveDrivingModels", "test", "viz", "test_viz_roadways.jl"))
--- test/viz/test_viz_roadways.jl
+++
@@ -1,4 +0,0 @@
-let
-    roadway = gen_stadium_roadway(2)
-    render(roadway)
-end
