6f800e18a919689b4bf16fc728a980812d9d19f2
Tim Wheeler <wheelert@stanford.edu>
2016-09-12 12:33:42 -0700

    added StandardizingExtractor and SubsetExtractor

--- src/features/feature_extractors.jl
+++ src/features/feature_extractors.jl
@@ -1,16 +1,28 @@
 export
     AbstractFeatureExtractor,
     FeatureExtractor,
+    SubsetExtractor,
+    StandardizingExtractor,
 
-    pull_features!
+
+    pull_features!,
+    rec_length
 
 abstract AbstractFeatureExtractor
+rec_length(::AbstractFeatureExtractor) = 1 # length of the SceneRecord for best results
 Base.length(::AbstractFeatureExtractor) = error("Not Impemeneted")
 pull_features!{F<:AbstractFloat}(::AbstractFeatureExtractor, features::Vector{F}, rec::SceneRecord, roadway::Roadway, vehicle_index::Int) = error("Not Implemented")
 
+###############################################################
+# FeatureExtractor
+
 type FeatureExtractor <: AbstractFeatureExtractor
     features::Vector{AbstractFeature}
+    rec_length::Int
+
+    FeatureExtractor(features::Vector{AbstractFeature}, rec_length::Int=1) = new(features, rec_length)
 end
+rec_length(ext::FeatureExtractor) = ext.rec_length # length of the SceneRecord for best results
 Base.length(ext::FeatureExtractor) = length(ext.features)
 function pull_features!{F<:AbstractFloat}(ext::FeatureExtractor, features::Vector{F}, rec::SceneRecord, roadway::Roadway, vehicle_index::Int)
 
@@ -18,6 +30,43 @@ function pull_features!{F<:AbstractFloat}(ext::FeatureExtractor, features::Vecto
     #            as such, this will be slow
     for (i,f) in enumerate(ext.features)
         features[i] = convert(Float64, get(f, rec, roadway, vehicle_index))
+    end
+    features
+end
+
+###############################################################
+# SubsetExtractor
+
+type SubsetExtractor{F<:AbstractFeatureExtractor, G<:AbstractFloat} <: AbstractFeatureExtractor
+    extractor::F
+    subset::Vector{Int}
+    features::Vector{G}
+end
+SubsetExtractor{G<:AbstractFloat}(extractor::AbstractFeatureExtractor, subset::Vector{Int}, ::Type{G}=Float64) = SubsetExtractor(extractor, subset, Array(G, length(extractor)))
+rec_length(ext::SubsetExtractor) = rec_length(ext.extractor)
+Base.length(ext::SubsetExtractor) = length(ext.subset)
+function pull_features!{F<:AbstractFloat}(ext::SubsetExtractor, features::Vector{F}, rec::SceneRecord, roadway::Roadway, vehicle_index::Int)
+    pull_features!(ext.extractor, ext.features, rec, roadway, vehicle_index)
+    for (i,j) in enumerate(ext.subset)
+        features[i] = ext.features[j]
+    end
+    features
+end
+
+###############################################################
+# StandardizingExtractor
+
+type StandardizingExtractor{F<:AbstractFeatureExtractor} <: AbstractFeatureExtractor
+    extractor::F
+    μ::Vector{Float64}
+    σ::Vector{Float64}
+end
+rec_length(ext::StandardizingExtractor) = rec_length(ext.extractor)
+Base.length(ext::StandardizingExtractor) = length(ext.extractor)
+function pull_features!{F<:AbstractFloat}(ext::StandardizingExtractor, features::Vector{F}, rec::SceneRecord, roadway::Roadway, vehicle_index::Int)
+    pull_features!(ext.extractor, features, rec, roadway, vehicle_index)
+    for i in 1 : length(features)
+        features[i] = (features[i] - ext.μ[i]) / ext.σ[i]
     end
     features
 end
--- test/test_feature_extractors.jl
+++ test/test_feature_extractors.jl
@@ -11,10 +11,25 @@ let
         ]))
 
     ext = DumbFeatureExtractor()
+    @test rec_length(ext) == 1
     @test_throws Exception length(ext)
     @test_throws Exception pull_features!(ext, Array(Float64, 0), rec, roadway, 1)
 
-    ext = FeatureExtractor(AbstractFeature[SPEED, POSFT])
+    ext = FeatureExtractor(AbstractFeature[SPEED, POSFT], 2)
+    @test rec_length(ext) == 2
     @test length(ext) == 2
     @test pull_features!(ext, [NaN,NaN], rec, roadway, 1) == [10.0, 0.0]
+
+    subset_ext = SubsetExtractor(ext, [1])
+    @test rec_length(subset_ext) == 2
+    @test length(subset_ext) == 1
+    @test pull_features!(subset_ext, [NaN], rec, roadway, 1) == [10.0]
+
+    subset_ext.subset[1] = 2
+    @test pull_features!(subset_ext, [NaN], rec, roadway, 1) == [0.0]
+
+    stan_ext = StandardizingExtractor(ext, [1.0, 2.0], [1.0, 4.0])
+    @test rec_length(stan_ext) == 2
+    @test length(stan_ext) == 2
+    @test pull_features!(stan_ext, [NaN,NaN], rec, roadway, 1) == [(10.0-1.0)/1.0, (0.0-2.0)/4.0]
 end
