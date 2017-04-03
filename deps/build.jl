64f3ea762c4346cca249254ec366d830498c151c
Tim Wheeler <tawheeler@users.noreply.github.com>
2017-04-03 09:34:50 -0700

    Update build.jl

--- deps/build.jl
+++ deps/build.jl
@@ -1,2 +1,22 @@
-Pkg.clone("https://github.com/tawheeler/Vec.jl.git")
-Pkg.clone("https://github.com/tawheeler/Records.jl.git")
+# clone
+urls = [
+    "https://github.com/tawheeler/Vec.jl.git",
+    "https://github.com/tawheeler/Records.jl.git",
+]
+
+for url in urls
+    try
+        Pkg.clone(url)
+    catch e
+        println("Exception when cloning $(url): $(e)")  
+    end
+end
+
+# checkout specific branches
+# checkouts = [
+#     ("ForwardNets", "nextgen")
+# ]
+
+# for (pkg, branch) in checkouts
+#     Pkg.checkout(pkg, branch)
+# end
