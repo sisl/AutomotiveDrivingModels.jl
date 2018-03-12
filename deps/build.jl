cfbafc273610109adf59f5169aff583e65b48db7
Maxime <MaximeBouton@users.noreply.github.com>
2018-03-11 23:03:24 -0700

    update build.jl

--- deps/build.jl
+++ deps/build.jl
@@ -1,8 +1,8 @@
 packages = keys(Pkg.installed())
 if !in("Vec", packages)
-    Pkg.clone("https://github.com/tawheeler/Vec.jl.git")
+    Pkg.clone("https://github.com/sisl/Vec.jl.git")
 end
 if !in("Records", packages)
-    Pkg.clone("https://github.com/tawheeler/Records.jl.git")
+    Pkg.clone("https://github.com/sisl/Records.jl.git")
 end
 
