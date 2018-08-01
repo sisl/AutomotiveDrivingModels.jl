a5e4c2d18880c070eba930b38c05f3a2c10a0909
MaximeBouton <maxim.bouton@gmail.com>
2018-08-01 16:00:44 -0700

    checkout Vec 0_6

--- deps/build.jl
+++ deps/build.jl
@@ -2,6 +2,8 @@ packages = keys(Pkg.installed())
 if !in("Vec", packages)
     Pkg.clone("https://github.com/sisl/Vec.jl.git")
 end
+warn("Checking out Vec.jl branch 0_6 for compatibility with julia 0.6")
+Pkg.checkout("Vec", "0_6")
 if !in("Records", packages)
     Pkg.clone("https://github.com/sisl/Records.jl.git")
 end
