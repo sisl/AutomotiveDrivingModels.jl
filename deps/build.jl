8586c03f825c83efe6ce01f06caed4ca9fb71c4b
MaximeBouton <maxim.bouton@gmail.com>
2018-08-21 15:56:48 -0700

    build script

--- deps/build.jl
+++ deps/build.jl
@@ -1,8 +1,8 @@
 using Pkg
 packages = keys(Pkg.installed())
 if !in("Vec", packages)
-    Pkg.add("https://github.com/sisl/Vec.jl.git")
+    Pkg.add(PackageSpec(url="https://github.com/sisl/Vec.jl.git"))
 end
 if !in("Records", packages)
-    Pkg.add("https://github.com/sisl/Records.jl.git")
+    Pkg.add(PackageSpec(url="https://github.com/sisl/Records.jl.git"))
 end
