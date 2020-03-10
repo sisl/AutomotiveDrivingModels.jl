766f0ae1aca2360bd587269e2e63bfec0cc1ca6a
Alexander Koufos <exoticdft@gmail.com>
2020-03-09 17:37:48 -0700

    Issue #56: Removes the need for external Records.jl package
    - Removes the external Records import in the ADM package.
    - Exports all the "records" functions, types, etc.
    - Removes need for calling "Records" module in interface.jl.

--- src/AutomotiveDrivingModels.jl
+++ src/AutomotiveDrivingModels.jl
@@ -7,8 +7,58 @@ using StaticArrays
 using Distributions
 using Reexport
 using Random
-@reexport using Vec 
-@reexport using Records
+@reexport using Vec
+
+# Records
+export
+    Entity,
+    Frame,
+    EntityFrame,
+    RecordFrame,
+    RecordState,
+    ListRecord,
+    QueueRecord,
+    EntityQueueRecord,
+
+    ListRecordFrameIterator,
+    ListRecordStateByIdIterator,
+
+    get_statetype,
+    get_deftype,
+    get_idtype,
+
+    capacity,
+    nframes,
+    nstates,
+    nids,
+    frame_inbounds,
+    pastframe_inbounds,
+    n_objects_in_frame,
+    id2index,
+    get_ids,
+    nth_id,
+    get_state,
+    get_def,
+    get_time,
+    get_timestep,
+    get_elapsed_time,
+    get_subinterval,
+    get_by_id,
+    findfirst_stateindex_with_id,
+    findfirst_frame_with_id,
+    findlast_frame_with_id,
+    get_first_available_id,
+    push_back_records!,
+    update!,
+    allocate_frame,
+    get_sparse_lookup
+
+include("records/common.jl")
+include("records/entities.jl")
+include("records/frames.jl")
+include("records/listrecords.jl")
+include("records/queuerecords.jl")
+include("records/conversions.jl")
 
 # Roadways
 
