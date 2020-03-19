1bfe7e6eb872f99c9491e492e7e1117136d49d31
MaximeBouton <maxim.bouton@gmail.com>
2020-03-19 14:59:55 -0700

    use reexport and add Vec tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -2,11 +2,17 @@ using Test
 using AutomotiveDrivingModels
 using Distributions
 
-include("test_roadways.jl")
-include("test_agent_definitions.jl")
-include("test_states.jl")
-include("test_collision_checkers.jl")
-include("test_actions.jl")
-include("test_features.jl")
-include("test_behaviors.jl")
-include("test_simulation.jl")
+@testset "Vec" begin 
+    include("vec-tests/vec_runtests.jl")
+end
+
+@testset "AutomotiveDrivingModels" begin 
+    include("test_roadways.jl")
+    include("test_agent_definitions.jl")
+    include("test_states.jl")
+    include("test_collision_checkers.jl")
+    include("test_actions.jl")
+    include("test_features.jl")
+    include("test_behaviors.jl")
+    include("test_simulation.jl")
+end
